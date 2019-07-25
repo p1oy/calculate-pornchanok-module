6428c74ba38abd93342d5d45d8eba344e1ba23a1
Pornchanok <pornchanok.s@kerrylogistics.com>
2019-07-25 12:04:07 +0700

    test pass

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const cal = require('./index');
 console.log(cal.plus(10,2));
 console.log(cal.subtract(10,2));
-console.log(cal.multiply(10,2))
+console.log(cal.multiply(10,2));
+console.log(cal.divide(10,2));
